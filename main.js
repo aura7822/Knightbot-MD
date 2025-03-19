e07a4c88efb378527bf50c19fd13064005522545
aura <joshuaura7822@gmail.com>
2025-03-19 17:19:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ const { autoStatusCommand, handleStatusUpdate } = require('./commands/autostatus
 global.packname = settings.packname;
 global.author = settings.author;
 global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
+global.ytch = "AURA && JULIA";
 
 // Add this near the top of main.js with other global configurations
 const channelInfo = {
@@ -84,7 +84,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterName: 'AURA && JULIA BOT',
             serverMessageId: -1
         }
     }
