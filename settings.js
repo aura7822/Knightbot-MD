0c82c078d753f9a3509ad67beb37758723a14420
aura <joshuaura7822@gmail.com>
2025-03-19 17:22:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Aura',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botName: "Aura && julia",
+  botOwner: 'Aura', // Your name
   ownerNumber: '917023951514', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
